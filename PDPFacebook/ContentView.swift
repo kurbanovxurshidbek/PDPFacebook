8d93e8da862171ceaaa691305ecde484ad9fba65
Khurshid <shamsun.com@gmail.com>
2020-11-01 14:56:59 +0900

    Initial Commit

---
+++ PDPFacebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDPFacebook
+//
+//  Created by KHURSHIDBEK on 2020/11/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
